a6ff0bd408eacf8ef7c37aa327608cab2d43404b
ZEMOURI Hocem <hocem.zemouri-extern@renault.com>
2018-07-06 14:37:57 +0200

    Before routing chapter

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +10,23 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
-  constructor() { }
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+                      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    // console.log(hero.name + ' was selected');
+    this.selectedHero = hero;
+  }
 }
